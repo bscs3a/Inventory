425011b2b76cda18b5b180ad069974d5f63452fb
Jake Ashley Panlilio <2021300973@dhvsu.edu.ph>
2024-03-17 20:01:54 +0800

    Update total_stock.php

--- public/inventory/functions/total_stock.php
+++ public/inventory/functions/total_stock.php
@@ -1,10 +1,11 @@
 <?php
 require_once 'db.php';
 
-$query = "SELECT * FROM total_stocks";
+$query = "SELECT * FROM total_stocks ORDER BY date_added DESC";
 
 $stmt = $conn->prepare($query);
 $stmt->execute();
 $rowsTStock = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 
 
