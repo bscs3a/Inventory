d943318e43a1fec692de89c8b8cb463bc93b525f
Jake Ashley Panlilio <2021300973@dhvsu.edu.ph>
2024-06-05 00:51:05 +0800

    reset

--- public/humanResources/js/sidenav-active-inactive.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.sidebar-toggle').addEventListener('click', function() {
-    document.getElementById('sidebar-menu').classList.toggle('hidden');
-    document.getElementById('sidebar-menu').classList.toggle('transform');
-    document.getElementById('sidebar-menu').classList.toggle('-translate-x-full');
-    document.getElementById('mainContent').classList.toggle('md:w-full');
-    document.getElementById('mainContent').classList.toggle('md:ml-64');
-});
--- public/humanResources/views/inc/noResult.php
+++
@@ -1,3 +0,0 @@
-<div class="ml-6 flex flex-col mt-8 mr-6 text-center">
-    No Record/s.
-</div>
